0410d7d0c1af470562ecc104af4d07b9c190e786
syscilythedeveloper <sbrown@syscilythedeveloper.com>
2024-01-23 17:55:33 -0500

    Adding nutrirecipe link

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -16,7 +16,27 @@ const Portfolio = () => {
       <h2>Selected Projects</h2>
 
       <div className="container portfolio__container">
-        {/* Project 1 */}
+
+      <div className='project'>
+          <h5>Recipe Finder </h5>
+          <div className="project__description">
+            <p>NutriRecipe is a user-friendly application designed to cater to anyone seeking culinary inspiration. Effortlessly search for recipes based on desired ingredients, macronutrients, and dietary preferences.</p>
+          
+          </div>
+        </div>
+
+        <article className="portfolio__item">
+          <div className="portfolio__item-image">
+            <img src={nutrirecipe} alt='landingpage' />
+          </div>
+          <div className='links'>
+            <a href="https://github.com/syscilythedeveloper/RecipeRefactor" className='btn btn-primary' target="_blank" rel="noopener noreferrer">Github</a>
+            <a href="https://nutrirecipe-sb.com/" className='btn btn-primary' target="_blank" rel="noopener noreferrer">Live Demo</a>
+          </div>
+        </article>
+        
+   
+        <AiOutlineArrowRight className='portfolio__icon' />
         <div className='project'>
           <h5>Game Hub</h5>
           <div className="project__description">
@@ -33,29 +53,9 @@ const Portfolio = () => {
             <a href="https://game-hub-five-dusky.vercel.app/" className='btn btn-primary' target="_blank" rel="noopener noreferrer">Live Demo</a>
           </div>
         </article>
-        <AiOutlineArrowRight className='portfolio__icon' />
 
-        {/* Project 2 (Example) */}
-        <div className='project'>
-          <h5>Recipe Finder </h5>
-          <div className="project__description">
-            <p>NutriRecipe is a user-friendly application designed to cater to anyone seeking culinary inspiration. Effortlessly search for recipes based on desired ingredients, macronutrients, and dietary preferences.</p>
-          
-          </div>
-        </div>
-
-        <article className="portfolio__item">
-          <div className="portfolio__item-image">
-            <img src={nutrirecipe} alt='landingpage' />
-          </div>
-          <div className='links'>
-            <a href="https://github.com/syscilythedeveloper/RecipeRefactor" className='btn btn-primary' target="_blank" rel="noopener noreferrer">Github</a>
-           
-          </div>
-        </article>
        
       </div>
-
       <div className='github__link'>
         <a href="https://github.com/syscilythedeveloper/NutriRecipe/blob/main/README.md" className='btn btn-primary' target="_blank" rel="noreferrer">Check out my Github!</a>
       </div>
