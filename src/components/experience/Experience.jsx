ed7077ed677c0140c4e80946ad358928d2ef71fa
syscilythedeveloper <sbrown@syscilythedeveloper.com>
2024-07-23 05:07:27 -0400

    resume

--- src/components/experience/Experience.jsx
+++ src/components/experience/Experience.jsx
@@ -1,161 +1,162 @@
-import React from 'react'
-import './experience.css'
-import {BsFillPatchCheckFill} from 'react-icons/bs'
+import React from "react";
+import "./experience.css";
+import { BsFillPatchCheckFill } from "react-icons/bs";
 
 const Experience = () => {
   return (
-    <section id='experience'>
-    <h5> My Skills</h5>
-    <h2> Experience</h2>
+    <section id="experience">
+      <h5> My Skills</h5>
+      <h2> Experience</h2>
 
-    <div className="container experience__container">
+      <div className="container experience__container">
+        <div className="experience_frontend">
+          <div className="experience__title">
+            <h3>Frontend Development</h3>
+          </div>
+          <div className="experience__content">
+            <article className="experience__details">
+              <BsFillPatchCheckFill className="experience__details-icon" />
+              <h4> HTML </h4>
+            </article>
 
-      <div className="experience_frontend">
-        <div className="experience__title">
-        <h3>Frontend Development</h3>
-        </div>
-        <div className="experience__content">
-
-          <article className = 'experience__details'>
-            <BsFillPatchCheckFill className="experience__details-icon" />
-                 <h4> HTML </h4>              
-          </article>
-          
-          <article className = 'experience__details'>
-            <BsFillPatchCheckFill className="experience__details-icon" />
+            <article className="experience__details">
+              <BsFillPatchCheckFill className="experience__details-icon" />
               <h4> CSS </h4>
-          </article>
+            </article>
 
-          <article className = 'experience__details'>
-            <BsFillPatchCheckFill className="experience__details-icon" />
+            <article className="experience__details">
+              <BsFillPatchCheckFill className="experience__details-icon" />
               <h4> Javascript </h4>
-          </article>
+            </article>
 
-          <article className = 'experience__details'>
-            <BsFillPatchCheckFill className="experience__details-icon" />
+            <article className="experience__details">
+              <BsFillPatchCheckFill className="experience__details-icon" />
               <h4> TypeScript </h4>
-          </article>
+            </article>
 
-          <article className = 'experience__details'>
-            <BsFillPatchCheckFill className="experience__details-icon" />
+            <article className="experience__details">
+              <BsFillPatchCheckFill className="experience__details-icon" />
               <h4> React.js </h4>
-          </article>
+            </article>
 
-          <article className = 'experience__details'>
-            <BsFillPatchCheckFill className="experience__details-icon" />
-                <h4> Bootstap </h4>  
-          </article>
+            <article className="experience__details">
+              <BsFillPatchCheckFill className="experience__details-icon" />
+              <h4> Bootstap </h4>
+            </article>
 
-          <article className = 'experience__details'>
-            <BsFillPatchCheckFill className="experience__details-icon" />
+            <article className="experience__details">
+              <BsFillPatchCheckFill className="experience__details-icon" />
               <h4> JQuery </h4>
-          </article>
+            </article>
 
-          <article className = 'experience__details'>
-            <BsFillPatchCheckFill className="experience__details-icon" />
+            <article className="experience__details">
+              <BsFillPatchCheckFill className="experience__details-icon" />
               <h4> Material-UI </h4>
-          </article>
+            </article>
 
-          <article className = 'experience__details'>
-            <BsFillPatchCheckFill className="experience__details-icon" />
+            <article className="experience__details">
+              <BsFillPatchCheckFill className="experience__details-icon" />
               <h4> Redux </h4>
-          </article>
+            </article>
 
-          <article className = 'experience__details'>
-            <BsFillPatchCheckFill className="experience__details-icon" />
+            <article className="experience__details">
+              <BsFillPatchCheckFill className="experience__details-icon" />
               <h4> Webpack </h4>
-          </article>
+            </article>
 
-          <article className = 'experience__details'>
-            <BsFillPatchCheckFill className="experience__details-icon" />
+            <article className="experience__details">
+              <BsFillPatchCheckFill className="experience__details-icon" />
               <h4> Git </h4>
-          </article>
+            </article>
 
-          <article className = 'experience__details'>
-            <BsFillPatchCheckFill className="experience__details-icon" />
+            <article className="experience__details">
+              <BsFillPatchCheckFill className="experience__details-icon" />
               <h4> Prettier </h4>
-          </article>
+            </article>
 
-          <article className = 'experience__details'>
-            <BsFillPatchCheckFill className="experience__details-icon" />
+            <article className="experience__details">
+              <BsFillPatchCheckFill className="experience__details-icon" />
               <h4> Vite </h4>
-          </article>
+            </article>
 
-          <article className = 'experience__details'>
-            <BsFillPatchCheckFill className="experience__details-icon" />
+            <article className="experience__details">
+              <BsFillPatchCheckFill className="experience__details-icon" />
               <h4> Tailwind </h4>
-          </article>
-
-        </div>
+            </article>
+          </div>
         </div>
 
         {/* End of Frontend */}
-   
 
-     
-      <div className="experience_backend">
-      <div className='experience__title'>
-      <h3 >Backend Development</h3>
-      </div>
-        <div className="experience__content">
-
-          <article className = 'experience__details'>
-            <BsFillPatchCheckFill className="experience__details-icon" />     
+        <div className="experience_backend">
+          <div className="experience__title">
+            <h3>Backend Development</h3>
+          </div>
+          <div className="experience__content">
+            <article className="experience__details">
+              <BsFillPatchCheckFill className="experience__details-icon" />
               <h4> Python </h4>
-          </article>
+            </article>
 
-          <article className = 'experience__details'>
-            <BsFillPatchCheckFill className="experience__details-icon" />     
+            <article className="experience__details">
+              <BsFillPatchCheckFill className="experience__details-icon" />
+              <h4> Express.js </h4>
+            </article>
+
+            <article className="experience__details">
+              <BsFillPatchCheckFill className="experience__details-icon" />
+              <h4> Mongoose </h4>
+            </article>
+
+            <article className="experience__details">
+              <BsFillPatchCheckFill className="experience__details-icon" />
               <h4> RESTful API </h4>
-          </article>
+            </article>
 
-          <article className = 'experience__details'>
-            <BsFillPatchCheckFill className="experience__details-icon" />     
+            <article className="experience__details">
+              <BsFillPatchCheckFill className="experience__details-icon" />
               <h4> SQLAlchemy </h4>
-          </article>
-          
-          <article className = 'experience__details'>
-            <BsFillPatchCheckFill className="experience__details-icon" />
+            </article>
+
+            <article className="experience__details">
+              <BsFillPatchCheckFill className="experience__details-icon" />
               <h4> PostgreSQL </h4>
-          </article>
+            </article>
 
-          <article className = 'experience__details'>
-            <BsFillPatchCheckFill className="experience__details-icon" />     
+            <article className="experience__details">
+              <BsFillPatchCheckFill className="experience__details-icon" />
               <h4> Agile Methodologies </h4>
-          </article>
+            </article>
 
-          <article className = 'experience__details'>
-            <BsFillPatchCheckFill className="experience__details-icon" />     
+            <article className="experience__details">
+              <BsFillPatchCheckFill className="experience__details-icon" />
               <h4> Node.js </h4>
-          </article>
+            </article>
 
-          <article className = 'experience__details'>
-            <BsFillPatchCheckFill className="experience__details-icon" />
-            <h4> Flask </h4>
-          </article>
+            <article className="experience__details">
+              <BsFillPatchCheckFill className="experience__details-icon" />
+              <h4> Flask </h4>
+            </article>
 
-          <article className = 'experience__details'>
-            <BsFillPatchCheckFill className="experience__details-icon" />
-            <h4> Jinja </h4>
-          </article>
+            <article className="experience__details">
+              <BsFillPatchCheckFill className="experience__details-icon" />
+              <h4> Jinja </h4>
+            </article>
 
-          <article className = 'experience__details'>
-            <BsFillPatchCheckFill className="experience__details-icon" />            
-            <h4> MongoDB </h4>
-          </article>
+            <article className="experience__details">
+              <BsFillPatchCheckFill className="experience__details-icon" />
+              <h4> MongoDB </h4>
+            </article>
 
-          <article className = 'experience__details'>
-            <BsFillPatchCheckFill className="experience__details-icon" />
-            <h4> Jasmine </h4>
-          </article>
-
+            <article className="experience__details">
+              <BsFillPatchCheckFill className="experience__details-icon" />
+              <h4> Jasmine </h4>
+            </article>
+          </div>
         </div>
-        
-        
-        </div>
-    </div>
+      </div>
     </section>
-  )
-}
+  );
+};
 
-export default Experience
+export default Experience;
--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,67 +1,131 @@
-import React from 'react'
-import './portfolio.css'
-import gamehub from "../../assets/gamehub.png"
-import nutrirecipe from "../../assets/nutrirecipe.png"
+import React from "react";
+import "./portfolio.css";
+import gamehub from "../../assets/gamehub.png";
+import nutrirecipe from "../../assets/nutrirecipe.png";
+import nomadic from "../../assets/nomadAIc.png";
 
-import {AiOutlineArrowRight} from 'react-icons/ai'
-
-
-
-
+import { AiOutlineArrowRight } from "react-icons/ai";
 
 const Portfolio = () => {
   return (
-    <section id='portfolio'>
+    <section id="portfolio">
       <h5>Portfolio</h5>
       <h2>Selected Projects</h2>
 
       <div className="container portfolio__container">
-
-      <div className='project'>
+        <div className="project">
           <h5>Recipe Finder </h5>
           <div className="project__description">
-            <p>NutriRecipe is a user-friendly application designed to cater to anyone seeking culinary inspiration. Effortlessly search for recipes based on desired ingredients, macronutrients, and dietary preferences.</p>
-          
+            <p>
+              NutriRecipe is a user-friendly application designed to cater to
+              anyone seeking culinary inspiration. Effortlessly search for
+              recipes based on desired ingredients, macronutrients, and dietary
+              preferences.
+            </p>
           </div>
         </div>
 
         <article className="portfolio__item">
           <div className="portfolio__item-image">
-            <img src={nutrirecipe} alt='landingpage' />
+            <img src={nutrirecipe} alt="landingpage" />
           </div>
-          <div className='links'>
-            <a href="https://github.com/syscilythedeveloper/RecipeRefactor" className='btn btn-primary' target="_blank" rel="noopener noreferrer">Github</a>
-            <a href="https://nutrirecipe-sb.com/" className='btn btn-primary' target="_blank" rel="noopener noreferrer">Live Demo</a>
+          <div className="links">
+            <a
+              href="https://github.com/syscilythedeveloper/RecipeRefactor"
+              className="btn btn-primary"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Github
+            </a>
+            <a
+              href="https://nutrirecipe-sb.com/"
+              className="btn btn-primary"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Live Demo
+            </a>
           </div>
         </article>
-        
-   
-        <AiOutlineArrowRight className='portfolio__icon' />
-        <div className='project'>
+
+        <AiOutlineArrowRight className="portfolio__icon" />
+
+        <div className="project">
+          <h5>NomadAIC </h5>
+          <div className="project__description">
+            <p>
+              An AI-powered mobile app used to automate itinerary generation for
+              a tailored and hassle-free planning experience
+            </p>
+          </div>
+        </div>
+
+        <article className="portfolio__item">
+          <div className="portfolio__item-image">
+            <img src={nomadic} alt="landingpage" />
+          </div>
+          <div className="links">
+            <a
+              href="https://lakshmiswathi293.wixstudio.io/nomadaic"
+              className="btn btn-primary"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Live Demo
+            </a>
+          </div>
+        </article>
+
+        <AiOutlineArrowRight className="portfolio__icon" />
+        <div className="project">
           <h5>Game Hub</h5>
           <div className="project__description">
-            <p>Unleash the power of gaming with the Gaming Hub — a dynamic project that revolutionizes your gaming experience. This application, driven by TypeScript, Chakra UI, and the rawg.io API, provides an intuitive platform for gamers.</p>
+            <p>
+              Unleash the power of gaming with the Gaming Hub — a dynamic
+              project that revolutionizes your gaming experience. This
+              application, driven by TypeScript, Chakra UI, and the rawg.io API,
+              provides an intuitive platform for gamers.
+            </p>
           </div>
         </div>
 
         <article className="portfolio__item">
           <div className="portfolio__item-image">
-            <img src={gamehub} alt='landingpage' />
+            <img src={gamehub} alt="landingpage" />
           </div>
-          <div className='links'>
-            <a href="https://github.com/syscilythedeveloper/game_app" className='btn btn-primary' target="_blank" rel="noopener noreferrer">Github</a>
-            <a href="https://game-hub-five-dusky.vercel.app/" className='btn btn-primary' target="_blank" rel="noopener noreferrer">Live Demo</a>
+          <div className="links">
+            <a
+              href="https://github.com/syscilythedeveloper/game_app"
+              className="btn btn-primary"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Github
+            </a>
+            <a
+              href="https://game-hub-five-dusky.vercel.app/"
+              className="btn btn-primary"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Live Demo
+            </a>
           </div>
         </article>
-
-       
       </div>
-      <div className='github__link'>
-        <a href="https://github.com/syscilythedeveloper/NutriRecipe/blob/main/README.md" className='btn btn-primary' target="_blank" rel="noreferrer">Check out my Github!</a>
+      <div className="github__link">
+        <a
+          href="https://github.com/syscilythedeveloper"
+          className="btn btn-primary"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Check out my Github!
+        </a>
       </div>
     </section>
-  )
-}
+  );
+};
 
-
-export default Portfolio
+export default Portfolio;
